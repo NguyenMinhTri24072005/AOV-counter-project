840a65afefbd53ddba7c4bc98cbfbbed1663831b
NguyenMinhTri24072005 <Nguyenminhtri2475@gmail.com>
2026-05-10 23:51:24 +0700

    khong co gi

--- combine.js
+++ combine.js
@@ -60,7 +60,7 @@ const rootDir = process.cwd();
 processDirectory(rootDir);
 
 const finalOutput = treeStructure + fileContents;
-const outputFileName = 'COMBINE_FOR_AI_Project.txt';
+const outputFileName = 'combined_Project.txt';
 
 fs.writeFileSync(outputFileName, finalOutput);
 console.log(`✅ Hoàn tất! Hãy gửi file "${outputFileName}" cho AI để bắt đầu làm việc.`);
